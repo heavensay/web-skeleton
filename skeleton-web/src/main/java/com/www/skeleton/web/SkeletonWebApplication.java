d0ea4a5f7104f73d559cb3e25ff35b61ee603a08
zny818@163.com <zny818@163.com>
2019-02-01 21:54:44 +0800

    fix instance can not found bug

--- skeleton-web/src/main/java/com/www/skeleton/web/SkeletonWebApplication.java
+++ skeleton-web/src/main/java/com/www/skeleton/web/SkeletonWebApplication.java
@@ -2,8 +2,10 @@ package com.www.skeleton.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.www.*")
 public class SkeletonWebApplication {
 
 	public static void main(String[] args) {
